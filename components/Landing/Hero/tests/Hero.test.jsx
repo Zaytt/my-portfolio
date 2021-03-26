c35cfe97669b47b769264a3c5fde321410e3e09b
Zaytt <ivanchavez.dev@gmail.com>
2021-03-25 18:48:47 -0600

    Test usern

--- components/Landing/Hero/tests/Hero.test.jsx
+++ components/Landing/Hero/tests/Hero.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import Hero from '../Hero';
 
 test('greeting message is displaying correctly', () => {
